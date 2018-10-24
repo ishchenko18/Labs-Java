bdbd9dbf86c14b5a4f5cc3a07fad6fe870901e99
Vladyslav Ishchenko <valdyslav.ishchenko99@gmail.com>
2018-10-24 23:56:49 +0300

    Changed concept of MVC in second lab.

--- src/main/java/second/Controller.java
+++ src/main/java/second/Controller.java
@@ -1,12 +1,17 @@
 package second;
 
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Controller {
     private Model model;
+    private View view;
+    private Scanner scanner;
 
     public Controller() {
         model = new Model();
+        view = new View();
+        scanner = new Scanner(System.in);
     }
 
     public void readStringFromFile(String fileName) {
@@ -29,6 +34,35 @@ public class Controller {
         model.changeString();
 
         return getText();
+    }
+
+    public void doTask() {
+        view.printInfoAboutDeveloper();
+
+        view.printString("\nPlease, choose method of initializing(file - f, console - c): ");
+        String method = scanner.next();
+
+        switch (method) {
+            case "f":
+                view.printString("Input fileName: ");
+                String fileName = scanner.next();
+                readStringFromFile(fileName);
+                view.printTextBeforeChanging(getText());
+                changeString();
+                view.printTextAfterChanging(getText());
+                break;
+            case "c":
+                view.printString("Input text(for finish reading enter 'end'): \n");
+                readStringFromConsole();
+                view.printTextBeforeChanging(getText());
+                changeString();
+                view.printTextAfterChanging(getText());
+                break;
+            default:
+                view.printString("Such variant method of reading does not exist.");
+                break;
+        }
+
     }
 
     public String getText() {
--- src/main/java/second/Main.java
+++ src/main/java/second/Main.java
@@ -1,38 +1,8 @@
 package second;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
         Controller controller = new Controller();
-        View view = new View();
-        view.printInfoAboutDeveloper();
-
-        view.printString("\nPlease, choose method of initializing(file - f, console - c): ");
-        String method = scanner.next();
-
-        switch (method) {
-            case "f":
-                view.printString("Input fileName: ");
-                String fileName = scanner.next();
-                controller.readStringFromFile(fileName);
-                view.printTextBeforeChanging(controller.getText());
-                controller.changeString();
-                view.printTextAfterChanging(controller.getText());
-                break;
-            case "c":
-                view.printString("Input text(for finish reading enter 'end'): \n");
-                controller.readStringFromConsole();
-                view.printTextBeforeChanging(controller.getText());
-                controller.changeString();
-                view.printTextAfterChanging(controller.getText());
-                break;
-            default:
-                view.printString("Such variant method of reading does not exist.");
-                break;
-        }
-
+        controller.doTask();
     }
 }
