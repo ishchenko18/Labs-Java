8be4f5628d04444382ce55379e1e943bee0f1ada
Vladyslav Ishchenko <valdyslav.ishchenko99@gmail.com>
2018-10-26 20:46:40 +0300

    Added method for reading information from console.

--- src/main/java/third/view/View.java
+++ src/main/java/third/view/View.java
@@ -3,15 +3,30 @@ package third.view;
 import third.entities.Flower;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class View {
-    public static void printList(List<Flower> flowers) {
+    private Scanner scanner;
+
+    public View() {
+        this.scanner = new Scanner(System.in);
+    }
+
+    public void printList(List<Flower> flowers) {
         for (Flower flower : flowers) {
             System.out.println(flower);
         }
     }
 
-    public static void printString(String string) {
+    public void printString(String string) {
         System.out.print(string);
+    }
+
+    public int inputNumber() {
+        return scanner.nextInt();
+    }
+
+    public String inputString() {
+        return scanner.next();
     }
 }
